ca52fad5a46b099a050b9d4494dcf14b54b8653e
Sophie Bonneval <maldorana@gmail.com>
2024-07-27 17:15:33 +0100

    Tweak styles and fonts

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import classes from './Header.module.scss';
 
 function Header() {
-    return(<div className={classes.container}>Header</div>);
+  return <div className={classes['container']}>Drabbler</div>;
 }
 
 export default Header;
