065d974b49c0c50f3c3911bee3d2f984dfe34006
Sophie Bonneval <maldorana@gmail.com>
2024-12-26 22:26:26 +0000

    Turn wordcounter green when 100 words and fix punctuation word counter issue

--- src/components/MainInput/MainInput.jsx
+++ src/components/MainInput/MainInput.jsx
@@ -5,14 +5,13 @@ import PropTypes from 'prop-types';
 import CopyButton from '../CopyButton/CopyButton';
 
 function MainInput({ inputText }) {
+  const initialValue = 'This is the initial content of the editor.';
   const editorRef = useRef(null);
-  const [editorValue, setEditorValue] = useState('');
+  const [editorValue, setEditorValue] = useState(initialValue);
 
   const handleEditorChange = (newValue, editor) => {
     setEditorValue(newValue);
-
-    // Whenever you change the text, you want the inputText prop to be updated:
-    // But first, you need to check its existence, because they're not mandatory
+    // Whenever you change the text, you want the inputText prop to be updated
     if (inputText) {
       inputText(editor.getContent({ format: 'text' }));
     }
@@ -40,7 +39,7 @@ function MainInput({ inputText }) {
         tinymceScriptSrc='/tinymce/tinymce.min.js'
         licenseKey='your-license-key'
         onInit={(_evt, editor) => (editorRef.current = editor)}
-        initialValue='<p>This is the initial content of the editor.</p>'
+        initialValue={initialValue}
         init={{
           height: 500,
           menubar: false,
--- src/components/WordCounter/WordCounter.jsx
+++ src/components/WordCounter/WordCounter.jsx
@@ -1,19 +1,38 @@
 import { useEffect, useState } from 'react';
 import classes from './WordCounter.module.scss';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 function WordCounter({ inputText }) {
   const [wordCount, setWordCount] = useState(0);
 
   // the useEffect will refresh whenever the value of the dependencies (second argum.) changes
   useEffect(() => {
-    if (inputText) {
-      setWordCount(inputText.trim().split(/\s+/).length);
+    const inputTextCleaned = inputText.trim();
+
+    if (inputTextCleaned) {
+      const wordCountValue = inputTextCleaned
+        .split(/\s+/)
+        .filter((word) => !word.match(/^[\p{P}]+$/u)).length;
+      setWordCount(wordCountValue);
+    } else {
+      setWordCount(0);
     }
   }, [inputText]);
 
   return (
-    <div className={classes['WordCounter__container']}>{wordCount} Words</div>
+    <div
+      className={classNames(classes['WordCounter__container'], {
+        [classes['WordCounter__green']]: wordCount === 100,
+      })}
+    >
+      {wordCount} Words
+    </div>
   );
 }
 
 export default WordCounter;
+
+WordCounter.propTypes = {
+  inputText: PropTypes.string,
+};
